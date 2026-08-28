e5b1c564e176dd184ae5077d61436f8cf4925117
wangyuling93 <trz5799tvf@privaterelay.appleid.com>
2026-08-28 14:03:45 +0800

    fix(console): type RequestEvent.locals for solid-js/web
    Unblock v2 pre-push typecheck after the solid-js client RequestEvent
    lost locals in its public types.

--- packages/console/app/src/global.d.ts
+++ packages/console/app/src/global.d.ts
@@ -1,5 +1,11 @@
 /// <reference types="@solidjs/start/env" />
 
+declare module "solid-js/web" {
+  interface RequestEvent {
+    locals: Record<string | number | symbol, any>
+  }
+}
+
 export declare module "@solidjs/start/server" {
   export type APIEvent = { request: Request }
 }
